61d6d6abc943786653f4e58e05fe6b22634e2164
Fabjan Sukalia <fsukalia@gmail.com>
2017-07-06 00:43:40 +0200

    change licence to GPLv3

--- src/arch/core.rs
+++ src/arch/core.rs
@@ -2,6 +2,22 @@
  * core.rs
  * Author: Fabjan Sukalia <fsukalia@gmail.com>
  * Date: 2016-11-04
+ * Copyright 2016-2017 Fabjan Sukalia
+ *
+ * This file is part of ruscy-v
+ *
+ * ruscy-v is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * ruscy-v is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ruscy-v.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #![allow(dead_code)]
--- src/arch/mod.rs
+++ src/arch/mod.rs
@@ -1,3 +1,24 @@
+/*
+ * mod.rs
+ * Author: Fabjan Sukalia <fsukalia@gmail.com>
+ * Date: 2016-11-04
+ * Copyright 2016-2017 Fabjan Sukalia
+ *
+ * This file is part of ruscy-v
+ *
+ * ruscy-v is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * ruscy-v is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with ruscy-v.  If not, see <http://www.gnu.org/licenses/>.
+ */
 pub mod decoder;
 mod constants;
 mod core;
